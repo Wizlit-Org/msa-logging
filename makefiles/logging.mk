ff9d0599acd46abfca287e12590bd9d9842eb3ae
hoban4336 <hoban4336@gmail.com>
2025-06-09 20:24:27 +0900

    grafana cloud

--- makefiles/logging.mk
+++ makefiles/logging.mk
@@ -1,7 +1,7 @@
 NAMESPACE ?= logging
 
 .PHONY deploy_alloy
-deploy_alloy:
+deploy_alloy: ## Install Grafana Alloy (Grafana Cloud)
 	helm repo add grafana https://grafana.github.io/helm-charts
 	helm repo update
 	helm upgrade --install alloy grafana/alloy \
