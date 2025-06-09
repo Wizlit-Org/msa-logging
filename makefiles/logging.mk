0244bc4b9c6110ad02f6f85735bb6940474cfb51
hoban4336 <hoban4336@gmail.com>
2025-06-09 20:23:44 +0900

    grafana cloud

--- makefiles/logging.mk
+++ makefiles/logging.mk
@@ -1,6 +1,9 @@
+NAMESPACE ?= logging
+
 .PHONY deploy_alloy
 deploy_alloy:
-
-	@helm repo add grafana https://grafana.github.io/helm-charts && \
-	helm repo udpate && \
-	helm install --namespace <NAMESPACE> <RELEASE_NAME> grafana/alloy
+	helm repo add grafana https://grafana.github.io/helm-charts
+	helm repo update
+	helm upgrade --install alloy grafana/alloy \
+	-n $(NAMESPACE) \
+	-f values-override.yaml
