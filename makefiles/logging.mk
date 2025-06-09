d61cea27cb5857d0d24521ddaa149df2eed6c4d9
hoban4336 <hoban4336@gmail.com>
2025-06-09 21:21:15 +0900

    grafana cloud

--- makefiles/logging.mk
+++ makefiles/logging.mk
@@ -1,14 +1,15 @@
 NAMESPACE = logging
 
 .PHONY: deploy_alloy
-deploy_alloy: create_ns ## Install Grafana Alloy (Grafana Cloud)
+deploy_alloy: set_secret ## Install Grafana Alloy (Grafana Cloud)
 	helm repo add grafana https://grafana.github.io/helm-charts
 	helm repo update
 	helm upgrade --install alloy grafana/alloy \
 	-n $(NAMESPACE) \
 	--create-namespace \
-	-f alloy/values-override.yaml
+	-f /tmp/values-override.yaml
+	@rm -f /tmp/values-override.yaml
 
-.PHONY: set_secretKey
-set_secretKey:  ## Secret Key 적용
-	sed 's|<API KEY>|$(ALLOY_API_KEY)|g' alloy/values-override-tmp.yaml > /alloy/values-override.yaml
+.PHONY: set_secret
+set_secret:
+	@sed 's|<API Key>|$(ALLOY_API_KEY)|g' values-override.yaml > /tmp/values-override.yaml
