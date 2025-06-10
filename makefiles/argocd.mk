b7dcdf5f66dc7a0ce0a9b1d2924ae3441a662ef9
hoban4336 <hoban4336@gmail.com>
2025-06-10 17:30:24 +0900

    argocd appofapp

--- makefiles/argocd.mk
+++ makefiles/argocd.mk
@@ -8,4 +8,4 @@ deploy_argocd: create_ns  ## argocd 설치
 	-n $(NAMESPACE) \
 	--create-namespace \
 	-f argocd/values-override.yaml
-	kubectl apply -n $(NAMESPACE) -f https://raw.githubusercontent.com/Wizlit-Org/msa-logging/refs/heads/main/argocd_apps/infra-dev.yaml
+	kubectl apply -n $(NAMESPACE) -f https://raw.githubusercontent.com/Wizlit-Org/msa-logging/refs/heads/main/argocd_apps/minikube/infra-dev.yaml
