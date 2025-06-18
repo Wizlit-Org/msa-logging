391782f544665fb48b683b9ce302dec8db0f61c3
hoban4336 <hoban4336@gmail.com>
2025-06-18 22:08:02 +0900

    knative

--- makefiles/knative.mk
+++ makefiles/knative.mk
@@ -1,9 +1,14 @@
 NAMESPACE = knative-operator
 
-.PHONY: deploy_knative
-deploy_knative: ## knative 설치
+.PHONY: deploy_knative_operator
+deploy_knative_operator:
 	@helm repo add knative-operator https://knative.github.io/operator && \
 	helm repo update && \
 	helm upgrade --install knative-operator knative-operator/knative-operator \
-	-n $(NAMESPACE) \
-	--create-namespace
+		-n knative-operator \
+		--create-namespace
+
+.PHONY: deploy_knative_cr
+deploy_knative_cr:
+	kubectl apply -f knative/knative-serving.yaml
+	kubectl apply -f knative/knative-eventing.yaml
